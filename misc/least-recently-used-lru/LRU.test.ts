22db69208c88172950181473088fc8ae039247f7
Mettle X <56449790+mettlex@users.noreply.github.com>
2023-06-18 00:35:19 +0600

    demo for array based lru

--- misc/least-recently-used-lru/LRU.test.ts
+++ misc/least-recently-used-lru/LRU.test.ts
@@ -6,19 +6,31 @@ type User = {
   password: string
 }
 
-const lru = createLRU<User>({ capacity: 4 })
+const lru = createLRU<User>({
+  capacity: 4,
+  findIndex: (user: User, sequence: User[]) => {
+    return sequence.findIndex((x) => x.username === user.username && x.password === user.password)
+  },
+  filter: (user: User, sequence: User[]) => sequence.filter(x => x.username !== user.username)
+})
 
-lru.set("1", { username: "Monir", password: "password1" })
-lru.set("2", { username: "Mahi", password: "password2" })
-lru.set("3", { username: "Azmain", password: "password3" })
-lru.set("4", { username: "Raiyan", password: "password4" })
+lru.set("Monir", { username: "Monir", password: "password1" })
+lru.set("Mahi", { username: "Mahi", password: "password2" })
+lru.set("Azmain", { username: "Azmain", password: "password3" })
+lru.set("Raiyan", { username: "Raiyan", password: "password4" })
 
 Deno.test("LRU get and set methods works", () => {
-  assertEquals(lru.get("2"), { username: "Mahi", password: "password2" });
+  assertEquals(lru.get("Mahi"), { username: "Mahi", password: "password2" });
 })
 
 Deno.test("Removes the least recently used", () => {
-  lru.set("5", { username: "Arabi", password: "password5" })
+  lru.set("Arabi", { username: "Arabi", password: "password5" })
 
-  assertEquals(lru.get("1"), undefined);
+  assertEquals(lru.get("Monir"), undefined);
+})
+
+Deno.test("Moves the most recently used", () => {
+  lru.get("Azmain")
+
+  assertEquals(lru.sequence[lru.sequence.length - 1], { username: "Azmain", password: "password3" })
 })
--- misc/least-recently-used-lru/LRU.ts
+++ misc/least-recently-used-lru/LRU.ts
@@ -1,5 +1,7 @@
-type CreateLRUParams = {
+type CreateLRUParams<T> = {
   capacity: number; // number of max items LRU can hold
+  findIndex: (item: T, sequence: T[]) => number
+  filter: (item: T, sequence: T[]) => T[]
 }
 
 type LinkedList<T> = unknown // TODO
@@ -7,32 +9,61 @@ type LinkedList<T> = unknown // TODO
 type LRU<DataType> = {
   capacity: number;
   size: number;
-  data: LinkedList<DataType>;
+  // sequence: LinkedList<DataType>;
+  sequence: DataType[];
   set: (key: string, value: DataType) => void;
   get: (key: string) => DataType | undefined;
 }
 
-export function createLRU<PassedDataType>({ capacity }: CreateLRUParams): LRU<PassedDataType> {
+export function createLRU<PassedDataType>({ capacity, findIndex, filter }: CreateLRUParams<PassedDataType>): LRU<PassedDataType> {
   const lookup = new Map<string, PassedDataType>();
+  let sequence: PassedDataType[] = [];
+
+  const prepend = (item: PassedDataType) => {
+    const itemIndex = findIndex(item, sequence)
+
+    if (itemIndex === sequence.length - 1) {
+      return
+    }
+
+    // if you use array, you have to do extra work here
+    sequence = filter(item, sequence)
+
+    sequence.push(item)
+  }
 
   const lru: LRU<PassedDataType> = {
     size: 0,
     capacity,
-    data: lookup, // TODO: make this linked list
+    sequence, // TODO: make this linked list
     set(key: string, value: PassedDataType) {
       lookup.set(key, value)
+      sequence.push(value)
 
       if (this.size + 1 > this.capacity) {
         // delete the least recently used
         // TODO: use Linked List
         const first = lookup.keys().next().value
         lookup.delete(first) // NAIVE: don't do this
+
+        sequence.shift()
       }
 
       this.size++;
     },
     get(key: string): PassedDataType | undefined {
-      return lookup.get(key)
+      const foundValue = lookup.get(key)
+
+      if (!foundValue) {
+        return undefined
+      }
+
+      // move item at first in sequence
+      prepend(foundValue)
+
+      this.sequence = sequence
+
+      return foundValue
     }
   }
 
