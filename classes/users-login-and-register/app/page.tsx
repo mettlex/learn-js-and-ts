4314c1f5b791d7eff0bab55adc09bd98f30d88da
Mettle X <56449790+mettlex@users.noreply.github.com>
2023-10-31 23:38:44 +0600

    init shadcn for form example

---
+++ classes/users-login-and-register/app/page.tsx
@@ -0,0 +1,9 @@
+import { ProfileForm } from "@/components/brand/form";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <ProfileForm />
+    </main>
+  );
+}
---
+++ classes/users-login-and-register/components/brand/form.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const minLength = 2;
+const maxLength = 50;
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(minLength, {
+      message: `Username must be at least ${minLength} characters.`,
+    })
+    .max(maxLength, {
+      message: `Username can't be more than ${maxLength} characters`,
+    }),
+});
+
+export function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Your username" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
